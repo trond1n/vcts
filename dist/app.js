eecd3c2499b28b1b32a3499823e53873f2daeec4
Yuri Trondin <lopata@2ch.ru>
2022-07-17 16:18:37 +0500

    first

---
+++ dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.App = void 0;
+class App {
+    run() { console.log('Donre'); }
+}
+exports.App = App;
+const app = new App;
+app.run();
---
+++ src/app.ts
@@ -0,0 +1,5 @@
+export class App {
+  run() { console.log('Done') }
+}
+const app = new App
+app.run()
